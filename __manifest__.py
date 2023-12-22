acc778b82edf1befeb5025d2de478caf58d56784
anouar_faraji <faraji.anouarr@gmail.com>
2023-12-22 21:15:06 +0100

    v1 , even it's not working

--- __manifest__.py
+++ __manifest__.py
@@ -5,11 +5,13 @@
     'summary': 'Hospital Management System for Odoo',
     'depends': ['base'],
     'data': [
+        'views/menu.xml',
         'security/ir.model.access.csv',
         'views/hospital_patient_views.xml',
         'views/hospital_doctor_views.xml',
         'views/hospital_appointment_views.xml',
         'views/hospital_medical_record_views.xml',
+        
         # Add other XML files if you have any
     ],
     'demo': [
