66c783d5fd294f136d9b81080caf0eb879bfc5e6
Wiran-Larbi <wiranlarbiofficial@gmail.com>
2023-12-24 17:59:59 +0100

    Modifying Appointment Model

--- models/hospital_appointment.py
+++ models/hospital_appointment.py
@@ -4,6 +4,7 @@ class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _description = 'Hospital Appointment'
 
+    name = fields.Char(string="Appointment ID", readonly=True, copy=True)
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
     doctor_id = fields.Many2one('hospital.doctor', string='Doctor', required=True)
     date_time = fields.Datetime(string='Date and Time', required=True)
@@ -13,3 +14,24 @@ class HospitalAppointment(models.Model):
         ('completed', 'Completed'),
         ('cancelled', 'Cancelled'),
     ], default='scheduled', string='Status')
+
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('hospital.appointment') or 'APPOINTMENT'
+        msg_body = 'Appointment created'
+        for msg in self:
+            msg.message_post(body=msg_body)
+        result = super(HospitalAppointment, self).create(vals)
+        return result
+    
+    def schedule(self):
+		self.write({'status': 'scheduled'})
+
+	def complete(self):
+		self.write({'status': 'completed'})
+
+	def cancel(self):
+		self.write({'status': 'cancelled'})
+
+    def print_prescription(self):
+		return self.env.ref('hospital-management.report_print_prescription').report_action(self)
